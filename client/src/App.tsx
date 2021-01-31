77bfa2cb7d62dad51726d9332fddd1e21e73a742
Robert Stein <rstein92@gmail.com>
2021-01-31 00:14:11 -0800

    client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Api} from './_proto/service_pb_service'
+import {CreateGameRequest} from './_proto/service_pb'
+import {grpc} from "@improbable-eng/grpc-web";
+
+const Host = "https://localhost:3000"
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="button" value="New Tic Tac Toe Game" onClick={()=>{
+        const req = new CreateGameRequest()
+        req.setNumberOfPlayers(2)
+        grpc.unary(Api.CreateGame, {
+          request: req,
+          host: Host,
+          onEnd: (res) => {
+            const { status, statusMessage, headers, message, trailers } = res;
+            console.log("CreateGame.onEnd.status", status, statusMessage);
+            console.log("CreateGame.onEnd.headers", headers);
+            if (status === grpc.Code.OK && message) {
+              console.log("CreateGame.onEnd.message", message.toObject());
+            }      
+            console.log("CreateGame.onEnd.trailers", trailers);
+          }
+        })
+      }} />
     </div>
   );
 }
